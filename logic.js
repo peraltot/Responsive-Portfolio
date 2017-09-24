3df44373d9aaf5a5866ae26eb6f5c746d8614152
peraltot <tom.peralto@yahoo.com>
2017-09-23 22:11:37 -0400

    mystuff

---
+++ logic.js
@@ -0,0 +1,12 @@
+
+// When the textPink button is pressed...
+$("#textLinken").on("click", function(https://www.linkedin.com/m/login/) 
+
+// When the textOrange button is pressed...
+$("#textyahoo").on("click", function(tom.peralto@yahoo.com) 
+
+// When the textGreen button is pressed...
+$("#textGreen").on("click", function() {
+	// Change funText to green.
+	$( https://www.linkedin.com/m/login/)
+})
